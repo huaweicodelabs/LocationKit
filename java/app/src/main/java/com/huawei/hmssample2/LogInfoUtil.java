bd54e01b251384dd6503de09c3a153f00448ef83
xwr22 <73109104+xwr22@users.noreply.github.com>
2021-07-19 16:09:10 +0800

    Update LogInfoUtil.java

--- java/app/src/main/java/com/huawei/hmssample2/LogInfoUtil.java
+++ java/app/src/main/java/com/huawei/hmssample2/LogInfoUtil.java
@@ -218,7 +218,6 @@ public class LogInfoUtil {
             try {
                 locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
             } catch (Settings.SettingNotFoundException e) {
-                e.printStackTrace();
                 return false;
             }
             return locationMode != Settings.Secure.LOCATION_MODE_OFF;
